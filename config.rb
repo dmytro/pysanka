4b0ebb30a721294ed674d11033a8d20b9c604bcf
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-11-26 14:19:35 +0900

    Move layout to middleman - WIP

--- config.rb
+++ config.rb
@@ -1,2 +1,21 @@
 require 'slim'
 Slim::Engine.disable_option_validator!
+
+
+activate :livereload
+
+activate :i18n
+
+activate :directory_indexes
+
+set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true
+set :markdown_engine, :redcarpet
+
+configure :development do
+  activate :relative_assets
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
