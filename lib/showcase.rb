7ebf176926ea1fd3d79ef9ffe5769e7688c8d4a6
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-12-02 11:06:20 +0900

    Data for showcase display,

--- lib/showcase.rb
+++ lib/showcase.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Showcase < Middleman::Extension
   def initialize(app, options_hash={}, &block)
     super
@@ -16,33 +17,55 @@ class Showcase < Middleman::Extension
       Dir.glob("#{path}/*").select { |fn| File.directory?(fn) }
     end
 
+    def defaults
+      {
+        title: "Title",
+        subtitle: "Sub Title",
+        summary: "Що небудь про писанку...",
+        link_to: "/"
+      }
+    end
     def items
       dirs.map do |dir|
         item = ::Showcase::Item.new(dir)
-        {
+        OpenStruct.new({
           dir: dir,
             images: item.images,
             thumb: item.thumb,
-            asset_path: item.asset_path
-        }
+            asset_path: item.asset_path,
+            data: OpenStruct.new(defaults.merge(item.data))
+
+        })
       end
     end
   end
 
   class Item
 
-    attr_reader :path
+    attr_reader :path, :data
 
     def initialize(path)
       @path = path
+    end
+
+    def data_file
+      @data_file ||= "#{path}/data.yml"
+    end
+
+    def data
+      @data ||= (File.exists?(data_file) ? YAML.load_file(data_file) : {  })
     end
 
     def asset_path
       @asset_path ||= path.gsub("source/images/","")
     end
 
-    def images
+    def image_files
       @images ||= Dir.entries("#{ path }").grep(/\.jpg$/i )
+    end
+
+    def images
+      image_files.map { |x| "#{ asset_path }/#{ x }" }
     end
 
     def thumbs
@@ -57,7 +80,7 @@ class Showcase < Middleman::Extension
 
   helpers do
 
-    def showcase_items(limit=0)
+    def showcase(limit=0)
       ::Showcase::Items.new.items[0..(limit-1)]
     end
 
