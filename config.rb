84f48098a0722795862022544528cb3801f411d3
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-12-03 15:34:36 +0900

    Start using localizations

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 require 'slim'
 Slim::Engine.disable_option_validator!
 
+#$. << "lib"
+require "#{File.dirname(__FILE__)}/lib/showcase"
+
 # general settings
 set :encoding, 'utf-8'
 set :index_file, 'index.html'
@@ -12,10 +15,10 @@ set :haml, { :format => :html5 }
 # don't precompile assets when middleman startup, but only when it's requested.
 set :debug_assets, true
 
-
+activate :showcase
 activate :livereload
 
-activate :i18n
+activate :i18n, langs: [:en, :uk, :ja]
 
 activate :directory_indexes
 
@@ -26,10 +29,25 @@ configure :development do
   activate :relative_assets
 end
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+helpers do
+  def current_language
+    data.languages[I18n.locale]
+  end
+
+  # Get the current page URL without locale prepended
+  def current_without_locale
+    current_page.path.sub!("^\/?#{I18n.locale}\/", "")
+  end
+
+  def current_with_locale(locale)
+    case locale
+    when 'en'
+      "#{current_without_locale}"
+    else
+      "#{locale}/#{current_without_locale}"
+    end
+  end
+end
 
 configure :build do
 #  activate :directory_indexes
