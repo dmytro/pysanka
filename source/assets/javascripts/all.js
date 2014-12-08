ddd63f0761a665b13b58b8946a4fddbd427ef530
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2014-12-08 16:40:45 +0900

    Jump to top button.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -41,3 +41,24 @@ $(document).resize(function() {
     var bodyheight = $(window).height();
     $("#sequence").height(bodyheight);
 });
+
+
+// $(document).ready(function() {
+//     $("#toTop").css("display", "none");
+//     $(window).scroll(function(){
+//         if($(window).scrollTop() > 0){
+//             console.log("is more");
+//             $("#toTop").fadeIn("slow");
+//         }
+//         else {
+//             console.log("is less");
+//             $("#toTop").fadeOut("slow");
+
+//         }
+//     });
+
+//     $("#toTop").click(function(){
+//         event.preventDefault();
+//         $("html, body").animate({ scrollTop:0 }, "slow");
+//     });
+// );
